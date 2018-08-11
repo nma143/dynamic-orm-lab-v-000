65b940c46e598c2ac780088a571e98b68daa4cc7
Nikki A <nikki.apostolakis@gmail.com>
2018-08-11 01:32:08 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -48,7 +48,7 @@ class InteractiveRecord
     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
     DB[:conn].execute(sql)
   end
-  def self.find_by(criteria={})  
+  def self.find_by(criteria={})
     criteria.each do |property, value|
       sql = "SELECT * FROM #{self.table_name} WHERE #{property} = '#{value}'"
       DB[:conn].execute(sql)
