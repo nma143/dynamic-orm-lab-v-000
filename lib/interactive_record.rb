abb0319fe7d2e0935d123c5a339c37cd7525f916
Nikki A <nikki.apostolakis@gmail.com>
2018-08-11 02:07:23 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -50,7 +50,7 @@ class InteractiveRecord
   end
   def self.find_by(property)
     column = property.keys.first.to_s
-    value = property[property.keys]
+    value = property[property.keys.first]
     sql = "SELECT * FROM #{self.table_name} WHERE #{column} = '#{value}'"
     puts "sql: #{sql}"
     DB[:conn].execute(sql)
